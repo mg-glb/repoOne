4be5c4aafa52df2762428c6cb9f957a7ccc9e421
m.gigena <m.gigena@globant.com>
2017-03-09 11:32:20 -0300

    Adding depended class

---
+++ moduleTwo/src/main/java/io/repoOne/moduleTwo/BasePage.java
@@ -0,0 +1,9 @@
+package io.repoOne.moduleTwo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+  private WebDriver driver;
+  private WebElement webElement;
+}
