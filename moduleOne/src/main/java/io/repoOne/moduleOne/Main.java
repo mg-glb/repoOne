3731777d0a8d067ed5bc4d1450916b8524b6ed9f
m.gigena <m.gigena@globant.com>
2017-03-09 14:04:29 -0300

    Adding new jar config

--- moduleOne/src/main/java/io/repoOne/moduleOne/Main.java
+++ moduleOne/src/main/java/io/repoOne/moduleOne/Main.java
@@ -1,8 +1,25 @@
 package io.repoOne.moduleOne;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws MalformedURLException {
     System.out.println("Loading jar...");
+    URLClassLoader loader = new URLClassLoader(
+        new URL[] {new URL("file://C:/Users/m.gigena/.m2/repository/io/repoOne/moduleTwo/1.0-SNAPSHOT/moduleTwo-1.0-SNAPSHOT.jar")},
+        Thread.currentThread().getContextClassLoader());
+    for (URL it : loader.getURLs()) {
+      System.out.println(it.toString());
+    }
+    try {
+      Class clazz = Class.forName("io.repoOne.moduleTwo.BasePage", true, loader);
+      System.out.println(clazz.getName());
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
