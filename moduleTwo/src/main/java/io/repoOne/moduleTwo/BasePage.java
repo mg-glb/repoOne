d96e20d710ac5a75879937a4a1bf15bd78a2fdb8
m.gigena <m.gigena@globant.com>
2017-03-09 11:43:13 -0300

    Adding methods

--- moduleTwo/src/main/java/io/repoOne/moduleTwo/BasePage.java
+++ moduleTwo/src/main/java/io/repoOne/moduleTwo/BasePage.java
@@ -1,9 +1,18 @@
 package io.repoOne.moduleTwo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasePage {
   private WebDriver driver;
   private WebElement webElement;
+  
+  public void navigateDummy(String address, String selector){
+    driver = new ChromeDriver();
+    driver.navigate().to(address);
+    webElement = driver.findElement(By.cssSelector(selector));
+    webElement.click();
+  }
 }
